45deb692fa8631743dfb8bc8a95ab2bfe0aee5d2
sivagnani <merugagnani@gmail.com>
2023-01-02 15:10:40 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise
+{
+    public class Sample1
+    {
+        public static int Test()
+        {
+            int x = 12 * 30;
+            Console.WriteLine(x);
+            return 0;
+        }
+    }
+    public class Sample2
+    {
+        public static int Test()
+        {
+            Console.WriteLine(FI(30));
+            Console.WriteLine(FI(120));
+            return 0;
+        }
+        static int FI(int x)
+        {
+            return x * 12;
+        }
+    }
+}
---
+++ Test2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Practise;
+class Test2
+{
+	static void Main()
+	{
+		Sample1.Test();
+        Sample2.Test();
+    }
+}
